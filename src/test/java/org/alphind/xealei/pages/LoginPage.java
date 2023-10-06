b0c5a5e405da1765eae9cc0694266b5047984b9c
Alphi-Razul <mohamedrazul.s@alphind.com>
2023-10-06 18:06:39 +0530

    Jenkins

--- src/test/java/org/alphind/xealei/pages/LoginPage.java
+++ src/test/java/org/alphind/xealei/pages/LoginPage.java
@@ -27,7 +27,7 @@ public class LoginPage extends BaseClass {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(xpath = "//p[text()='Powered by Alphind © 2023']")
+	@FindBy(xpath = "//p[contains(text(),'Powered by Alphind']")
 	private WebElement valForLoginScreen;
 	
 	@FindBy(xpath = "//h3[text()='LOGIN']")
@@ -51,7 +51,7 @@ public class LoginPage extends BaseClass {
 	@FindBy(xpath = "//h1[text()='PRIVACY POLICY']")
 	private WebElement privacyPolicyContent;
 
-	@FindBy(xpath = "//p[text()='Powered by Alphind © 2023']")
+	@FindBy(xpath = "//p[contains(text(),'Powered by Alphind']")
 	private WebElement bottomText;
 
 	@FindBy(xpath = "//input[@placeholder='Email']")
